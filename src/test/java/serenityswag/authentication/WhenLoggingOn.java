76542ee9ec1234017c40da25f862658a8291451c
fazugo1 <fazugo1@gmail.com>
2026-01-05 16:17:49 -0500

    se implemente junit5 usando find

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -1,6 +1,7 @@
 package serenityswag.authentication;
 
 import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.thucydides.core.annotations.findby.By;
@@ -12,23 +13,34 @@ import org.openqa.selenium.WebDriver;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SerenityJUnit5Extension.class)
-public class WhenLoggingOn {
+public class WhenLoggingOn extends UIInteractionSteps {
 
     @Managed
     WebDriver driver;
     @Test
     public void usuarioPuedeLoguearseenHomePage(){
-       driver.get("https://www.saucedemo.com/");
+    //   driver.get("https://www.saucedemo.com/");
+         openUrl("https://www.saucedemo.com/");
+
        // iniciar sesion con usuario específico
-        //data-test="username"
-         driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
-         driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
-         driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+        // driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
+         //driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
+         //driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+
+         find("[data-test='username']").sendKeys("standard_user");
+         find("[data-test='password']").sendKeys("secret_sauce");
+         find("[data-test='login-button']").click();
+
+
 
          // entrar a ver los productos
-       // driver.findElement(By.cssSelector("[data-test='title']")).getText();
-        assertThat(driver.findElement(By.cssSelector(".title")).getText())
-                .isEqualToIgnoringCase("Products");
+       // assertThat(driver.findElement(By.cssSelector("[data-test='title']")).getText())
+         //           .isEqualToIgnoringCase("Products");
+       // assertThat(driver.findElement(By.cssSelector(".title")).getText())
+       //         .isEqualToIgnoringCase("Products");
+
+       assertThat(find(".title").getText()).isEqualToIgnoringCase("Products");
+
     }
 
 }
