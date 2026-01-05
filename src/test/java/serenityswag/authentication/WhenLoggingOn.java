f5e266c02f9b1da54609a40ddbe734295d620555
fazugo1 <fazugo1@gmail.com>
2026-01-05 15:56:18 -0500

    caso de test basico

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -1,10 +1,12 @@
 package serenityswag.authentication;
 
 import net.serenitybdd.annotations.Managed;
-import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import net.thucydides.core.annotations.findby.By;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,19 +16,19 @@ public class WhenLoggingOn {
 
     @Managed
     WebDriver driver;
-
     @Test
-    public void usersCanLogOnViaTheHomePage() {
+    public void usuarioPuedeLoguearseenHomePage(){
+       driver.get("https://www.saucedemo.com/");
+       // iniciar sesion con usuario específico
+        //data-test="username"
+         driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
+         driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
+         driver.findElement(By.cssSelector("[data-test='login-button']")).click();
 
-        driver.get("https://www.saucedemo.com");
-
-        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
-
-        // Should see product catalog
-
+         // entrar a ver los productos
+       // driver.findElement(By.cssSelector("[data-test='title']")).getText();
         assertThat(driver.findElement(By.cssSelector(".title")).getText())
                 .isEqualToIgnoringCase("Products");
     }
+
 }
---
+++ src/test/java/serenityswagOld/authenticationOld/WhenLoggingOnOld.java
@@ -0,0 +1,32 @@
+package serenityswagOld.authenticationOld;
+
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SerenityJUnit5Extension.class)
+public class WhenLoggingOnOld {
+
+    @Managed
+    WebDriver driver;
+
+    @Test
+    public void usersCanLogOnViaTheHomePage() {
+
+        driver.get("https://www.saucedemo.com");
+
+        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+
+        // Should see product catalog
+
+        assertThat(driver.findElement(By.cssSelector(".title")).getText())
+                .isEqualToIgnoringCase("Products");
+    }
+}
