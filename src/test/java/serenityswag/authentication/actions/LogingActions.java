b935f6e6eb737254fd1cc7b5e251f77db499de0f
fazugo1 <fazugo1@gmail.com>
2026-01-10 17:07:17 -0500

    ajuste de serenity config y Find By id

--- src/test/java/serenityswag/authentication/actions/LogingActions.java
+++ src/test/java/serenityswag/authentication/actions/LogingActions.java
@@ -2,43 +2,36 @@ package serenityswag.authentication.actions;
 
 import net.serenitybdd.annotations.Step;
 import net.serenitybdd.core.steps.UIInteractionSteps;
+import net.thucydides.core.annotations.findby.By;
 import serenityswag.authentication.Users;
 
 public class LogingActions extends UIInteractionSteps {
 
     @Step("login con usuario {0}")
    /* public void asStandarUser() {
-
         openUrl("https://www.saucedemo.com/");
         // iniciar sesion con usuario standar
         $("[data-test='username']").sendKeys("standard_user");
         $("[data-test='password']").sendKeys("secret_sauce");
         $("[data-test='login-button']").click();
-
-
-
     }
-
-    //*******************Usando parámetros************
-
-
-    public void usingCredendtials(String user, String password) {
-
+//*******************Usando parámetros************
+   public void usingCredendtials(String user, String password) {
         openUrl("https://www.saucedemo.com/");
         // iniciar sesion con usuario standar
         $("[data-test='username']").sendKeys(user);
         $("[data-test='password']").sendKeys(password);
         $("[data-test='login-button']").click();
     }
-
-    //*****Refactoring Using credentials********
+ //*****Refactoring Using credentials********
 
    */
     public void as(Users user)  {
 
         openUrl("https://www.saucedemo.com/");
         // iniciar sesion con usuario standar
-        $("[data-test='username']").sendKeys(user.getUsername());
+        //$("[data-test='username']").sendKeys(user.getUsername());
+        find(By.id("user-name")).sendKeys(user.getUsername());
         $("[data-test='password']").sendKeys(user.getPassword());
         $("[data-test='login-button']").click();
     }
